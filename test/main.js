b094b79561bc79034b1acb4cdd4f985c1b3a7c3c
Damon Oehlman <damon.oehlman@sidelab.com>
2012-09-24 14:48:07 +1000

    Added before and after handlers for test initialization

--- test/main.js
+++ test/main.js
@@ -2,6 +2,14 @@ var testrest = require('../'),
     server = require('./server');
     
 describe('testREST functionality tests', function() {
-    describe('GET tests', testrest('get'));
-    describe('PUT tests', testrest('put'));
+    describe('GET tests', testrest('get', {
+        before: server.listen.bind(server, 3000),
+        after: server.close.bind(server)
+    }));
+    
+    
+    describe('PUT tests', testrest('put', {
+        before: server.listen.bind(server, 3000),
+        after: server.close.bind(server)
+    }));
 })
--- test/server/index.js
+++ test/server/index.js
@@ -6,30 +6,30 @@ var tako = require('tako'),
 app.route('/').text('Hi');
 
 app.route('/json-test').json({
-    first: 'Roger',
-    last: 'Rabbit'
+  first: 'Roger',
+  last: 'Rabbit'
 });
 
 app.route('/empty').json([]);
 
 app.route('/empty-fail').json({
-    error: 'failed'
+  error: 'failed'
 });
 
 app.route('/test.txt')
-  .text(function (req, res) {
-    fs.readFile(path.resolve(__dirname, '..', 'data', 'test.txt'), 'utf8', function(err, data) {
-        res.end(data);
-    });
-  })
-  .methods('GET');
+.text(function (req, res) {
+  fs.readFile(path.resolve(__dirname, '..', 'data', 'test.txt'), 'utf8', function(err, data) {
+      res.end(data);
+  });
+})
+.methods('GET');
 
 app.route('/test')
-  .json(function(req, res) {
-    req.on('json', function(obj) {
-      res.end(obj);
-    });
-  })
-  .methods('PUT');
+.json(function(req, res) {
+  req.on('json', function(obj) {
+    res.end(obj);
+  });
+})
+.methods('PUT');
 
-app.httpServer.listen(3000);
+module.exports = app.httpServer;
