7013087ef2d0b1c77e50bc2f1b2b16a44eea7b21
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-12 17:21:00 +1000

    gendocs

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# testREST - REST API Testing Solution
+# testrest
 
-testREST is a testing helper that is designed to make the process of testing your REST application easier to write and easier to read. A simple [YAML](http://www.yaml.org/) definition file format is used to describe tests.  For example:
+testREST is a testing helper that is designed to make the process of testing
+your REST application easier to write and easier to read. A simple
+[YAML](http://www.yaml.org/) definition file format is used to describe tests.
+
+For example:
 
 ```yaml
 - it: should be able to send headers to a server
   get: /echo-headers
-
+  
   headers:
     foo: bar
-
+    
   expect:
     status: 200
     body:
@@ -16,17 +20,19 @@ testREST is a testing helper that is designed to make the process of testing you
 
 - it: should be able to send cookies to a server
   get: /echo-cookies
-
+  
   cookies:
     foo: bar
-
+    
   expect:
     status: 200
     body:
       foo: bar
 ```
 
-Now the above tests a designed to test a simple server that echos header values and cookie values back in a JSON response, and when run in conjuction with the following mocha test file, will execute successfully:
+Now the above tests a designed to test a simple server that echos header values
+and cookie values back in a JSON response, and when run in conjuction with the
+following mocha test file, will execute successfully:
 
 ```js
 var testrest = require('testrest');
@@ -36,4 +42,27 @@ describe('header tests', testrest('headers', {
 }));
 ```
 
-There are a few things going on here, which will be explained in more detail in the upcoming testREST docs.  In the meantime, feel free to have a look at the tests in this repository or hack through the code.
+## License(s)
+
+### MIT
+
+Copyright (c) 2014 Damon Oehlman <damon.oehlman@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-var debug = require('debug')('testrest')
-  , path = require('path')
-  , assert = require('assert')
-  , yaml = require('js-yaml')
-  , fs = require('fs')
-  , util = require('util')
-  , defaultOpts = {
-      baseUrl: 'http://localhost:3000'
-    , engine: 'mocha'
-    }
-  , _ = require('underscore')
-  , _existsSync = fs.existsSync || path.existsSync
-  , testrequest = require('./lib/testrequest')
-  , ignoreKeys = [].concat(testrequest.methodNames);
+var debug = require('debug')('testrest');
+var path = require('path');
+var assert = require('assert');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var util = require('util');
+var _ = require('underscore');
+var _existsSync = fs.existsSync || path.existsSync;
+var testrequest = require('./lib/testrequest');
+var ignoreKeys = [].concat(testrequest.methodNames);
+
+var defaultOpts = {
+  baseUrl: 'http://localhost:3000',
+  engine: 'mocha'
+};
+
+/**
+  # testrest
+
+  testREST is a testing helper that is designed to make the process of testing
+  your REST application easier to write and easier to read. A simple
+  [YAML](http://www.yaml.org/) definition file format is used to describe tests.
+
+  For example:
+
+  <<< test/headers.yaml
+
+  Now the above tests a designed to test a simple server that echos header values
+  and cookie values back in a JSON response, and when run in conjuction with the
+  following mocha test file, will execute successfully:
+
+  <<< test/headers.js
+
+**/
 
 /**
 ## checkForServer
 
-This function inspects the opts that have been provided to testrest and determines whether a 
+This function inspects the opts that have been provided to testrest and determines whether a
 server has been specified for use.
 */
 function checkForServer(opts) {
   var serverInstance;
-  
+
   // check for a server instance
   if (opts.server && opts.server.listen && opts.server.close) {
     serverInstance = opts.server;
   }
-  
+
   // if we have a server instance, then create start and stop bindings
   if (serverInstance) {
     opts.before = [
       serverInstance.listen.bind(serverInstance, opts.port || 3000)
     ].concat(opts.before || []);
-    
+
     opts.after = [
       function(cb) {
         // close the server instance
         serverInstance.close();
-        
+
         // if the server instance is still running, then wait for the close event
         if (serverInstance.address()) {
           serverInstance.once('close', cb);
@@ -62,17 +82,17 @@ This function is used to expand file references that are encountered in the suit
 function expandFileReferences(data, basePath) {
   var targetPath
     , stats;
-  
+
   // iterate through the keys in the data
   _.each(data, function(value, key) {
     // if the key is in any of the ignore keys, then skip this check
     if (ignoreKeys.indexOf(key) >= 0) return;
-    
-    // if the value is a string, then we are going to have to take a 
+
+    // if the value is a string, then we are going to have to take a
     // look and see if it references a file
     if (typeof value == 'string' || (value instanceof String)) {
       targetPath = path.resolve(basePath, value);
-      
+
       // if this is a valid path, then load the file
       debug('checking to see if "' + value + '" is a file: ' + targetPath);
       if (_existsSync(targetPath)) {
@@ -81,7 +101,7 @@ function expandFileReferences(data, basePath) {
         if (stats.isFile()) {
           debug('attempting to extract data for key "' + key + '" from file: ' + targetPath);
           value = fs.readFileSync(targetPath, 'utf8');
-          
+
           // try json parsing it
           try {
             value = JSON.parse(value);
@@ -92,7 +112,7 @@ function expandFileReferences(data, basePath) {
         }
       }
     }
-    
+
     // if the value is an object and not a string, then recurse
     if (typeof value == 'object' && (! (value instanceof String))) {
       data[key] = expandFileReferences(value, basePath);
@@ -102,8 +122,8 @@ function expandFileReferences(data, basePath) {
       data[key] = value;
     }
   });
-  
-  // return the data 
+
+  // return the data
   return data;
 }
 
@@ -114,11 +134,11 @@ This function is used to load a test suite from a specified definition file.
 function loadSuite(definitionFile, opts) {
   var ext = path.extname(definitionFile)
     , suite = require(definitionFile);
-    
+
   // TODO: attempt to load a parser first, and if not available
   // fall back to requiring the file
-  
-  // if test data is an array, then create a top level testdata object 
+
+  // if test data is an array, then create a top level testdata object
   // and insert the items as the tests member
   if (Array.isArray(suite)) {
     suite = {
@@ -126,19 +146,19 @@ function loadSuite(definitionFile, opts) {
     , tests: [].concat(suite)
     }
   }
-  
+
   // iterate through the tests and normalize the tests
   suite.tests.forEach(function(testdata, index) {
     // create a new test object and merge the test data into it
     var test = suite.tests[index] = testrequest.create();
-    
+
     // update the baseUrl of the test
     test.baseUrl = opts.baseUrl;
-    
+
     // update the test request from the data loaded from the definition file
     _.extend(test, expandFileReferences(testdata, path.dirname(definitionFile)));
   });
-  
+
   return suite;
 }
 
@@ -155,7 +175,7 @@ function wrapTasks(fns) {
         cb();
       };
     }
-    
+
     return fn;
   });
 }
@@ -167,37 +187,37 @@ var testrest = module.exports = function(definitionFile, opts) {
   var content
     , parser
     , suite;
-  
+
   // initialise the definition file name
   definitionFile = path.resolve(
     path.dirname(module.parent.filename),
     definitionFile || 'api'
   );
-  
+
   // if an extension hasn't been provided, default to .txt
   if (path.extname(definitionFile) === '') {
     definitionFile += '.yaml';
   }
-  
+
   // load the control file content
   content = fs.readFileSync(definitionFile, 'utf8');
-  
+
   // initialise the opts
-  opts = _.extend({}, defaultOpts, opts);  
+  opts = _.extend({}, defaultOpts, opts);
 
   // load and normalize the rules
   suite = loadSuite(definitionFile, opts);
-  
+
   // extend the opts with the suite opts
   _.extend(opts, suite.opts);
-  
+
   // check the server opts
   opts = checkForServer(opts);
-  
+
   // ensure we have a before and after array
   opts.before = wrapTasks([].concat(opts.before || []));
   opts.after = wrapTasks([].concat(opts.after || []));
-  
+
   // produce the test engine specific output
   return require('./engines/' + opts.engine)(suite.tests, opts);
 };
